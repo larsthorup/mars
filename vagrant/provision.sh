a5e799411ae66f3ec1ac93a034761d1fd0d23ecd
Lars Thorup <lars@zealake.com>
2014-02-05 15:36:45 -0600

    npm run deploy node server.js does not run in background yet

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,3 +6,5 @@ apt-get install -y python-software-properties python g++ make >/dev/null 2>&1
 add-apt-repository -y ppa:chris-lea/node.js >/dev/null 2>&1
 apt-get update >/dev/null 2>&1
 apt-get install -y nodejs >/dev/null 2>&1
+
+node /vagrant/
