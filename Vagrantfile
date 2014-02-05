08e61e6dfc8d1bcb52566e072f49ff1a266ba2fc
Lars Thorup <lars@zealake.com>
2014-02-05 12:15:44 -0600

    npm run deploy vagrant up provision nodejs

---
+++ Vagrantfile
@@ -0,0 +1,5 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.provision "shell", path: "vagrant/provision.sh"
+end
---
+++ vagrant/provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Installing NodeJS..."
+apt-get update >/dev/null 2>&1
+apt-get install -y python-software-properties python g++ make >/dev/null 2>&1
+add-apt-repository -y ppa:chris-lea/node.js >/dev/null 2>&1
+apt-get update >/dev/null 2>&1
+apt-get install -y nodejs >/dev/null 2>&1
