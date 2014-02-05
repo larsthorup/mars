5ac71d92b87e641a3b08b4a385abccfde21ea65f
Lars Thorup <lars@zealake.com>
2014-02-05 10:38:09 -0600

    npm run start

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@
 var restify = require('restify');
 var controller = require('./controller/hello');
 
-var server = restify.createServer();
+var server = restify.createServer({
+    name: 'mars'
+});
 server.get('/hello/:name', controller.hello);
 server.head('/hello/:name', controller.hello);
 server.get(/\/static\/?.*/, restify.serveStatic({
