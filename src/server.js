4deb05a95e1b10f6c876580b0eef29b039d83463
Lars Thorup <lars@zealake.com>
2014-02-15 20:51:21 -0800

    more realistic coverage

--- src/server.js
+++ src/server.js
@@ -6,15 +6,22 @@
 var restify = require('restify');
 var controller = require('./controller/hello');
 
-var server = restify.createServer({
-    name: 'mars'
-});
-server.get('/hello/:name', controller.hello);
-server.head('/hello/:name', controller.hello);
-server.get(/\/static\/?.*/, restify.serveStatic({
-    directory: './static'
-}));
+function start() {
 
-server.listen(1719, function() {
-    console.log('%s listening at %s', server.name, server.url);
-});
+    var server = restify.createServer({
+        name: 'mars'
+    });
+    server.get('/hello/:name', controller.hello);
+    server.head('/hello/:name', controller.hello);
+    server.get(/\/static\/?.*/, restify.serveStatic({
+        directory: './static'
+    }));
+
+    server.listen(1719, function() {
+        console.log('%s listening at %s', server.name, server.url);
+    });
+}
+
+module.exports = {
+    start: start
+};
---
+++ test/server.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+var server = require('../src/server.js');
+
+describe('server', function(){
+    // ToDo: add tests
+});
+
